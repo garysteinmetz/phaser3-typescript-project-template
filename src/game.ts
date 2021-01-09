0fea0e7db40996863f423131c95d1efafc4885aa
Gary Steinmetz <garysteinmetz@yahoo.com>
2021-01-09 12:42:41 -0500

    Change - Add basic Phaser scenes and image effects

--- src/game.ts
+++ src/game.ts
@@ -1,46 +1,81 @@
 import 'phaser';
 
+let screen_width = 800;
+let screen_height = 600;
+
 export default class Demo extends Phaser.Scene
 {
     constructor ()
     {
-        super('demo');
+        super('s1');
     }
+
+    image1: Phaser.GameObjects.Image;
+    clickCount = 0;
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image('scene1', 'assets/scene1.png');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        this.image1 = this.add.image(screen_width/2, screen_height/2, 'scene1');
+        this.image1.setInteractive();
+        this.image1.on(Phaser.Input.Events.POINTER_DOWN,
+            () => {
+                this.clickCount++;
+                //
+                if (this.clickCount === 2) {
+                    console.log("Start Effect - Resize, Shrink By Half");
+                    this.image1.setScale(0.5);
+                } else if (this.clickCount === 3) {
+                    console.log("Start Effect - Rotate, Flip Image Over");
+                    this.image1.setRotation(Phaser.Math.DegToRad(180));
+                } else if (this.clickCount === 4) {
+                    console.log("Start Effect - Set Alpha, Make Half Invisible");
+                    this.image1.setAlpha(0.5);
+                } else if (this.clickCount === 5) {
+                    console.log("Start Effect - Set Horizontal, Move Image Right");
+                    this.image1.x = this.image1.x + 100;
+                } else if (this.clickCount === 6) {
+                    console.log("Start Effect - Change Scene, Display Next Scene");
+                    let scene2 = this.scene.add('s2', new AnotherScene(), false);
+                    this.scene.switch('s2');
+                } else {
+                    console.log("First Click - Do Nothing");
+                }
+            });
+    }
+}
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+class AnotherScene extends Phaser.Scene {
+    //
+    constructor ()
+    {
+        super('s2');
+    }
 
-        this.add.image(400, 300, 'libs');
+    image2: Phaser.GameObjects.Image;
 
-        const logo = this.add.image(400, 70, 'logo');
+    preload ()
+    {
+        this.load.image('scene2', 'assets/scene2.png');
+    }
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    create ()
+    {
+        //
+        this.image2 = this.add.image(screen_width/2, screen_height/2, 'scene2');
+        console.log("Scene 2 has been loaded");
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    width: screen_width,
+    height: screen_height,
     scene: Demo
 };
 
